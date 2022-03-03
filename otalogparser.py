f623a34994506f7cd6fcc6f2dde91f146125ee56
nyuszika7h <nyuszika7h@gmail.com>
2022-03-03 13:53:13 +0100

    Fancier output + compare SEP version to target version + misc cleanup

--- otalogparser.py
+++ otalogparser.py
@@ -7,7 +7,30 @@ import re
 import sys
 from pathlib import Path
 
+from packaging import version
 from pyasn1.codec.der.decoder import decode
+from rich import print
+
+
+def info(msg):
+    print(f"[bold white]ℹ️  {msg}[/bold white]")
+
+
+def success(msg):
+    print(f"[bold green]✅ {msg}[/bold green]")
+
+
+def warning(msg):
+    print(f"[bold yellow]⚠️  {msg}[/bold yellow]")
+
+
+def error(msg):
+    print(f"[bold red]❌ {msg}[/bold red]")
+
+
+def fatal(msg):
+    error(msg)
+    sys.exit(1)
 
 
 parser = argparse.ArgumentParser()
@@ -22,13 +45,13 @@ lines = Path(args.file).read_text().splitlines()
 
 for line in lines:
     if "Failed to load update brain trust cache" in line:
-        sys.exit("ERROR: Failed to load update brain trust cache. You are probably in a jailbroken state, reboot.")
+        fatal("Failed to load update brain trust cache. You are probably in a jailbroken state, reboot.")
 
 for line in lines:
     if "Enabling managed request" in line:
         break
 else:
-    print("WARNING: Managed request line not found in log file. Are you supervised?")
+    warning("Managed request line not found in log file. Are you supervised?")
 
 for line in lines:
     if "failed tss request:<<<<<<<<<<" in line:
@@ -39,10 +62,10 @@ for line in lines:
         break
 
 if not tss_request:
-    sys.exit("ERROR: Unable to find TSS request in log file")
+    fatal("Unable to find TSS request in log file")
 
 if "@BCert" not in tss_request:
-    sys.exit("ERROR: No BCert found in TSS request")
+    warning("No BCert found in TSS request. Are you supervised?")
 
 if args.print_bcert:
     print(base64.b64encode(tss_request["@BCert"]).decode())
@@ -51,16 +74,25 @@ asn1, _ = decode(tss_request["@BCert"])
 try:
     data = str(asn1[0][-1][-1][-1])
 except LookupError:
-    sys.exit("ERROR: Unable to find SEP version in BCert")
-sep_version = re.search(r"[\d.]+$", data)
-if sep_version:
-    print("SEP version:", sep_version.group())
+    warning("Unable to find SEP version in BCert")
+sep_version = None
+if m := re.search(r"[\d.]+$", data):
+    sep_version = m.group()
+    info(f"SEP version: [cyan]{sep_version}[/cyan]")
 else:
-    sys.exit("ERROR: Field does not contain SEP version?!")
+    warning("Field does not contain SEP version?!")
+
+if target_version := tss_request.get("ProductMarketingVersion"):
+    info(f"Target version: [cyan]{target_version}[/cyan]")
+    if sep_version:
+        if version.parse(target_version) >= version.parse(sep_version):
+            success("Target version is higher or equal to SEP version")
+        else:
+            fatal("Target version is lower than SEP version. Updating is not possible.")
 
 for line in lines:
     if "STATUS=" in line:
-        print(line)
+        info(f"TSS status: [cyan]{line[line.index('STATUS='):]}")
         break
 else:
-    sys.exit("ERROR: Unable to find TSS response line in log file")
+    warning("Unable to find TSS response line in log file")
